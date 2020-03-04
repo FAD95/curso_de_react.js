818e205722988a964e395538d67f48647a8d670b
Fadith Escorcia <feescorciam@protonmail.com>
2020-03-04 03:00:43 -0500

    Fix routes for gh-pages second try

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./Layout";
+import Home from "../pages/Home";
 import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
 import NotFound from "./NotFound";
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Switch>
+          <Route exact path="/curso_de_react.js/home" component={Home} />
           <Route exact path="/curso_de_react.js/badge" component={Badges} />
           <Route exact path="/curso_de_react.js/badges/new" component={BadgeNew} />
           <Route component={NotFound} />
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <main>
+                <h1>Home</h1>
+                <Link to="/badge">Hla</Link>
+            </main>
+         );
+    }
+}
+ 
+export default Home;
