09d176e1f6663ca730bc5682cc85e52f18ad9b03
Fadith Escorcia <feescorciam@protonmail.com>
2020-03-18 00:50:41 -0500

    Updates

---
+++ precache-manifest.96c2b6fd546a3bfe7c68b0257ee93fe5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e03f23895fca2bcb74ab2db2eca793d",
+    "url": "/curso_de_react.js/index.html"
+  },
+  {
+    "revision": "e35b772b8ec1deba918e",
+    "url": "/curso_de_react.js/static/css/2.29cb211a.chunk.css"
+  },
+  {
+    "revision": "89e99d8ca08f13be1112",
+    "url": "/curso_de_react.js/static/css/main.3dfc3ccc.chunk.css"
+  },
+  {
+    "revision": "e35b772b8ec1deba918e",
+    "url": "/curso_de_react.js/static/js/2.8d0a71a7.chunk.js"
+  },
+  {
+    "revision": "4c2fad67987984b4bb16b7ed4c80a1c3",
+    "url": "/curso_de_react.js/static/js/2.8d0a71a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e99d8ca08f13be1112",
+    "url": "/curso_de_react.js/static/js/main.3cb03ae5.chunk.js"
+  },
+  {
+    "revision": "8a5759c7626b6e40a479",
+    "url": "/curso_de_react.js/static/js/runtime-main.d3c113ad.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/curso_de_react.js/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "b447eaeddd2ca40130429705f9fdaf3d",
+    "url": "/curso_de_react.js/static/media/badge-header.b447eaed.svg"
+  },
+  {
+    "revision": "63998388cb8382cea83f06d7721c6812",
+    "url": "/curso_de_react.js/static/media/delete.63998388.png"
+  },
+  {
+    "revision": "88a212e27c6cb6e3c6528ee4995ad860",
+    "url": "/curso_de_react.js/static/media/edit.88a212e2.png"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/curso_de_react.js/static/media/home-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/curso_de_react.js/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "ed914b75e71893be8b64f2e0a805b0ca",
+    "url": "/curso_de_react.js/static/media/monstruos.ed914b75.png"
+  },
+  {
+    "revision": "a3848242afb57e61a8a9a4e992da25d9",
+    "url": "/curso_de_react.js/static/media/rickandmorty.a3848242.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/curso_de_react.js/static/media/stars.1d17360a.svg"
+  }
+]);
